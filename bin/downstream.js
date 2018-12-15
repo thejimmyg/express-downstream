db9672a34476c46a9a409c2264bbcd252c0305a9
James Gardner <james@jimmyg.org>
2018-12-15 11:13:51 +0000

    first commit

---
+++ bin/downstream.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mustache = require('mustache')
+const port = process.env.PORT || 8000
+
+app.get('*', (req, res) => {
+  const path = mustache.escape(req.path)
+  res.send(`<html>
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>${path}</p>
+  </body>
+</html>`)
+})
+
+process.on('SIGINT', function () {
+  console.log('Exiting ...')
+  process.exit()
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
